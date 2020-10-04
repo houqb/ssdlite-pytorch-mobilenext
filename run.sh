9a74294bdac857918d7f71eb548ab09715c9ee7b
Andrew-Qibin <andrewhoux@gmail.com>
2020-10-04 10:16:40 +0800

    delete irrelevant configs

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
-export NGPU=8
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python -m torch.distributed.launch --nproc_per_node=$NGPU train.py \
-    --config-file ./configs/mobilenext_ssd320_coco.yaml \
+export NGPU=4
+CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.launch --nproc_per_node=$NGPU train.py \
+    --config-file ./configs/mobilenext_ssd320_voc0712.yaml \
+    --eval_step 10000 \
     SOLVER.WARMUP_FACTOR 0.1333 \
-    SOLVER.WARMUP_ITERS 2000
+    SOLVER.WARMUP_ITERS 1000
